488194e423445943d79aeaf297cfb8b1ea261899
JacRay <64971844+JacRay@users.noreply.github.com>
2023-05-10 22:05:56 +0530

    Update sendDataWithIdentifier.cpp

--- sendDataWithIdentifier.cpp
+++ sendDataWithIdentifier.cpp
@@ -1,70 +1,58 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
 #include <SoftwareSerial.h>
-#include <TinyGPS++.h>
 
-// Define the WiFi credentials
-const char* wifiSSID = "keralavison";
-const char* wifiPassword = "12345678";
+// Replace with your Wi-Fi credentials
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
 
-// Define the GPS module identifier
-const char* gpsIdentifier = "GPS1";
+// Replace with your server's IP address and port number
+const char* server = "your_SERVER_IP_ADDRESS";
+const int port = 80;
 
-// Define the server URL
-const char* serverURL = "http://www.iottraffic.xyz/getDataWithidentifier.php";
+// Replace with your unique identifier for this vehicle
+const char* vehicle_id = "your_VEHICLE_ID";
 
-// Define the GPS module serial port
-SoftwareSerial gpsSerial(5, 4); // RX, TX
-
-// Initialize the GPS parser
-TinyGPSPlus gps;
+// GPS module serial interface configuration
+SoftwareSerial gpsSerial(D5, D6);
 
 void setup() {
-  // Initialize the serial port
+  // Start serial communication
   Serial.begin(9600);
 
-  // Initialize the GPS serial port
+  // Configure GPS module serial interface
   gpsSerial.begin(9600);
 
-  // Connect to the WiFi network
-  WiFi.begin(wifiSSID, wifiPassword);
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
+
+  Serial.println("Connected to Wi-Fi.");
 }
 
 void loop() {
-  // Parse the GPS data
-  while (gpsSerial.available() > 0) {
-    if (gps.encode(gpsSerial.read())) {
-      // Check if a GPS fix was obtained
-      if (gps.location.isValid()) {
-        // Build the HTTP POST request payload
-        String payload = "identifier=";
-        payload += gpsIdentifier;
-        payload += "&latitude=";
-        payload += gps.location.lat();
-        payload += "&longitude=";
-        payload += gps.location.lng();
-
-        // Send the HTTP POST request to the server
-        HTTPClient http;
-        http.begin(serverURL);
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-        int httpCode = http.POST(payload);
-        http.end();
-
-        // Check if the request was successful
-        if (httpCode == HTTP_CODE_OK) {
-          Serial.println("Location sent successfully");
-        } else {
-          Serial.print("Failed to send location data: ");
-          Serial.println(httpCode);
-        }
-      } else {
-        Serial.println("GPS fix not obtained");
-      }
-    }
+  // Read GPS data
+  String gpsData = "";
+  while (gpsSerial.available()) {
+    gpsData += char(gpsSerial.read());
   }
+
+  // Send GPS data to server
+  if (gpsData != "") {
+    String data = "vehicle_id=" + String(vehicle_id) + "&gps_data=" + gpsData;
+    HTTPClient http;
+    http.begin(server, port, "/receive_data.php");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    int httpCode = http.POST(data);
+    if (httpCode == HTTP_CODE_OK) {
+      String response = http.getString();
+      Serial.println(response);
+    }
+    http.end();
+  }
+
+  delay(1000);
 }
